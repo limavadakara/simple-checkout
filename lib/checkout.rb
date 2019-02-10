720e407f4d97281b11d968e223938724da688edc
Lima Vadakara <lima.vadakara@gmail.com>
2019-02-10 20:31:44 +0000

    Checkout gets created with and empty list of items and checkout allows user to scan items

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,5 +1,13 @@
 class Checkout
+  attr_reader :items
+  def initialize
+    @items = []
+  end
   def view_price(item)
     item.price
+  end
+
+  def scan(item)
+    @items << item
   end
 end
--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -1,6 +1,12 @@
 require 'checkout.rb'
 
 describe Checkout do
+
+  it 'gets created with an empty list of items' do
+    checkout = Checkout.new
+    expect(checkout).to have_attributes(:items => [])
+  end
+
   it 'responds to view price' do
     expect(subject).to respond_to(:view_price)
   end
@@ -9,5 +15,12 @@ describe Checkout do
     item = Item.new(10)
     expect(subject.view_price(item)).to eq 10
   end
+
+  it 'allows user to scan items' do
+    item = Item.new(10)
+    subject.scan(item)
+    expect(subject.items).to include(item)
+  end
+
 
 end
